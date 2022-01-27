c8afff2033e2018f1dea74b601fa2c3c64c2e53e
dkolot <dkolotsoftserveinc.com>
2022-01-27 19:28:34 +0200

    add hw-3

---
+++ models/contact.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+const Joi = require('joi');
+
+const contactSchema = Schema({
+  name: {
+    type: String,
+    required: [true, 'Set name for contact'],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const joiShema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.bool(),
+});
+
+const Contact = model('contact', contactSchema);
+
+module.exports = {
+  Contact,
+  schemas: {
+    joiShema: joiShema,
+  },
+};
